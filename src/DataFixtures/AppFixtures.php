b566d880b2cf06973a293ac03bf96f6a8c241da2
wings <eternity.mr8@gmail.com>
2020-02-09 00:45:37 +0330

    finish fixtures.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -28,13 +28,19 @@ class AppFixtures extends Fixture
      */
     private $manager;
 
+    /**
+     * AppFixtures constructor.
+     * @param ContainerInterface $container
+     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
     }
 
 
-
+    /**
+     * @param ObjectManager $manager
+     */
     public function load(ObjectManager $manager):void
     {
         $this->manager = $manager;
@@ -45,12 +51,14 @@ class AppFixtures extends Fixture
         $manager->flush();
         $this->loadProducts($manager);
         $manager->flush();
-//        $this->loadOffers($manager);
-//        $manager->flush();
+        $this->loadOffers($manager);
+        $manager->flush();
     }
 
 
-
+    /**
+     * @param ObjectManager $manager
+     */
     private function loadUsers(ObjectManager $manager):void{
         for ($i = 1; $i <= $this->container->getParameter('total_users_number'); $i++) {
             $user = new User();
@@ -59,6 +67,9 @@ class AppFixtures extends Fixture
         }
     }
 
+    /**
+     * @param ObjectManager $manager
+     */
     private function loadSources(ObjectManager $manager):void{
         $premiumSourcePossibility = $this->container->getParameter('premium_sources_possibility');
         for ($i = 1; $i <= $this->container->getParameter('total_sources_number'); $i++) {
@@ -70,6 +81,9 @@ class AppFixtures extends Fixture
         }
     }
 
+    /**
+     * @param ObjectManager $manager
+     */
     private function loadCustomers(ObjectManager $manager):void{
         for ($i = 1; $i <= $this->container->getParameter('total_customers_number'); $i++) {
             $customer = new Customer();
@@ -78,40 +92,94 @@ class AppFixtures extends Fixture
         }
     }
 
+    /**
+     * @param ObjectManager $manager
+     */
     private function loadProducts(ObjectManager $manager):void{
-        $totalCustomersNumber = $this->container->getParameter('total_customers_number');
+        list($customerMinId, $customerMaxId) = $this->getCustomerMinAndMax($manager);
         for ($i = 1; $i <= $this->container->getParameter('total_products_number'); $i++) {
-            $randomCustomerId = rand(1, $totalCustomersNumber);
+            $randomCustomerId = rand($customerMinId, $customerMaxId);
             $customer = $manager->getRepository(Customer::class)->find($randomCustomerId);
+            if($customer == null){
+                dd($customer, $randomCustomerId);
+            }
             $product = new Product();
-            $product->setName("{$customer}-product-{$i}");
+            $product->setName("product-{$i}");
             $product->setCustomer($customer);
             $manager->persist($product);
         }
     }
 
+    /**
+     * @param ObjectManager $manager
+     */
     private function loadOffers(ObjectManager $manager):void{
-        $totalSourcesNumber = $this->container->getParameter('total_sources_number');
-        $totalProductsNumber = $this->container->getParameter('total_products_number');
-        $totalUsersNumber = $this->container->getParameter('total_users_number');
+        list($sourceMinId, $sourceMaxId) = $this->getSourceMinAndMax($manager);
+        list($productMinId, $productMaxId) = $this->getProductMinAndMax($manager);
+        list($userMinId, $userMaxId) = $this->getUserMinAndMax($manager);
+
         for ($i = 1; $i <= $this->container->getParameter('total_offers_number'); $i++) {
 
             $offer = new Offer();
 
-            $randomSourceId = rand(1, $totalSourcesNumber);
+            $randomSourceId = rand($sourceMinId, $sourceMaxId);
             $source = $manager->getRepository(Source::class)->find($randomSourceId);
             $offer->setSource($source);
 
-            $randomProductId = rand(1, $totalProductsNumber);
+            $randomProductId = rand($productMinId, $productMaxId);
             $product = $manager->getRepository(Product::class)->find($randomProductId);
             $offer->setProduct($product);
 
-            $randomUserId = rand(1, $totalUsersNumber);
+            $randomUserId = rand($userMinId, $userMaxId);
             $user = $manager->getRepository(User::class)->find($randomUserId);
             $offer->setUser($user);
 
             $manager->persist($offer);
         }
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return array
+     */
+    private function getUserMinAndMax(ObjectManager $manager):array{
+        $totalUsersNumber = $this->container->getParameter('total_users_number');
+        $firstUser = $manager->getRepository(User::class)->findOneBy([],['id' => 'ASC']);
+        $firstUserId = $firstUser->getId();
+        return [$firstUserId, $firstUserId + $totalUsersNumber - 1];
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return array
+     */
+    private function getProductMinAndMax(ObjectManager $manager):array{
+        $totalProductsNumber = $this->container->getParameter('total_products_number');
+        $firstProduct = $manager->getRepository(Product::class)->findOneBy([],['id' => 'ASC']);
+        $firstId = $firstProduct->getId();
+        return [$firstId, $firstId + $totalProductsNumber - 1];
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return array
+     */
+    private function getSourceMinAndMax(ObjectManager $manager):array{
+        $totalSourcesNumber = $this->container->getParameter('total_sources_number');
+        $firstSource = $manager->getRepository(Source::class)->findOneBy([],['id' => 'ASC']);
+        $firstId = $firstSource->getId();
+        return [$firstId, $firstId + $totalSourcesNumber - 1];
+    }
+
+    /**
+     * @param ObjectManager $manager
+     * @return array
+     */
+    private function getCustomerMinAndMax(ObjectManager $manager):array {
+        $totalCustomersNumber = $this->container->getParameter('total_customers_number');
+        $firstCustomer = $manager->getRepository(Customer::class)->findOneBy([],['id' => 'ASC']);
+        $firstId = $firstCustomer->getId();
+        return [$firstId, $firstId + $totalCustomersNumber - 1];
     }
 
 }
